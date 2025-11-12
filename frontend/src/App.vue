6967ef02ea367b98d0951d15d31626ca11e671f9
wangkai <wangkai@eystar.com>
2025-11-12 18:24:59 +0800

    第7次提交

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,13 @@
+<template>
+  <div>
+    <div v-for="item of userData" :key="item.id">
+      {{ item.name }}
+    </div>
+  </div>
+</template>
+
 <script setup>
 import { onMounted, ref } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
 const userData = ref([]);
 
 onMounted(() => {
@@ -13,24 +20,4 @@ onMounted(() => {
 });
 </script>
 
-<template>
-  <div v-for="item of userData" :key="item.id">
-    {{ item.name }}
-  </div>
-  <h1>111</h1>
-</template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
