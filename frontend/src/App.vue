71bea346cf69acf9a85b26db4527757b26a49bb8
wangkai <wangkai@eystar.com>
2025-11-13 02:15:31 +0800

    31

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,7 +9,12 @@
 
 <script setup>
 import { onMounted, ref } from "vue";
-const userData = ref([{ id: 111, name: "占位符" }]);
+const userData = ref([
+  { id: 1, name: "张三" },
+  { id: 2, name: "李四" },
+  { id: 3, name: "王五" },
+  { id: 4, name: "赵六" },
+]);
 
 onMounted(() => {
   // fetch("http://localhost:3000/api/hello") // 确保这与你的 Node.js 服务端口匹配
